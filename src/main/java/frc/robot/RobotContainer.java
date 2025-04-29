8025effcb4e24f3911e4ab345ce53c680dbb471d
SuryanshSahitya <suryansh.sahitya@gmail.com>
2025-04-29 09:06:58 -0400

    Removed extra lines

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,7 +6,7 @@ package frc.robot;
 
 import frc.robot.commands.ArcadeDriveCmd;
 import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+//import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -26,7 +26,7 @@ import frc.robot.subsystems.DriveSubsystem;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  //private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
   private final CameraSubsystem CameraSubsystem = new CameraSubsystem();
   private final DriveSubsystem driveSubsystem = new DriveSubsystem();
   private final ShooterButtonSubsystem ShooterButtonSubsystem = new ShooterButtonSubsystem();
@@ -42,7 +42,7 @@ public class RobotContainer {
     configureBindings();
 
     
-      driveSubsystem.setDefaultCommand(new ArcadeDriveCmd(driveSubsystem,() -> controller_uno.getRawAxis(1), () -> controller_uno.getRawAxis(4), 0.6));
+      driveSubsystem.setDefaultCommand(new ArcadeDriveCmd(driveSubsystem,() -> controller_uno.getRawAxis(1), () -> controller_uno.getRawAxis(4), 0.8));
       controller_uno.b().whileTrue(ShooterButtonSubsystem.buttonClickerCommand(-4, 0.001, 1)).whileFalse(ShooterButtonSubsystem.buttonClickerCommand(0,0.001, 1));
 //      ShooterButtonSubsystem.ButtonMotorCommand(controller_uno.getRightTriggerAxis());
       }
@@ -59,8 +59,8 @@ public class RobotContainer {
    */
   private void configureBindings() {
     // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
+    // new Trigger(m_exampleSubsystem::exampleCondition)
+    //     .onTrue(new ExampleCommand(m_exampleSubsystem));
 
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
     // cancelling on release.
